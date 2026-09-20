1393d072ab0750871511c4e174813090add93015
SaridakisStamatisChristos <34583142+SaridakisStamatisChristos@users.noreply.github.com>
2026-09-20 07:04:07 +0300

    style(gui): order client resolver test imports

--- gui/src/util/clientTools/multiEditImpl.test.ts
+++ gui/src/util/clientTools/multiEditImpl.test.ts
@@ -1,8 +1,10 @@
 import { ContinueError, ContinueErrorReason } from "core/util/errors";
-import * as clientPathResolver from "./resolveClientToolPath";
 import { beforeEach, describe, expect, it, Mock, vi } from "vitest";
+
 import { applyForEditTool } from "../../redux/thunks/handleApplyStateUpdate";
+
 import { ClientToolExtras } from "./callClientTool";
+import * as clientPathResolver from "./resolveClientToolPath";
 import { multiEditImpl } from "./multiEditImpl";
 
 vi.mock("uuid", () => ({
